e8428366e697d89419df0159cc913e23c81b2db8
sks <sks5995study@gmail.com>
2025-06-04 11:10:38 +0900

    변경경 24

--- JavaScripts/popup.js
+++ JavaScripts/popup.js
@@ -1,7 +1,7 @@
 function pop_up() {
     var cookieCheck = getCookie("popupYN");
-    if (cookieCheck != "N"){
-        window.open("/java-main/html/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
+    if (cookieCheck != "N") {
+        window.open("./HTML/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
     }
 }
 
@@ -16,7 +16,7 @@ function getCookie(name) {
     console.log("쿠키를 요청합니다.");
     if (cookie != "") {
         var cookie_array = cookie.split("; ");
-        for ( var index in cookie_array) {
+        for (var index in cookie_array) {
             var cookie_name = cookie_array[index].split("=");
             if (cookie_name[0] == "popupYN") {
                 return cookie_name[1];
@@ -34,31 +34,31 @@ function closePopup() {
     }
 }
 
-function show_clock(){
+function show_clock() {
     let currentDate = new Date();
     let divClock = document.getElementById('divClock');
     let msg = "현재 시간 : ";
-    if(currentDate.getHours()>12){
+    if (currentDate.getHours() > 12) {
         msg += "오후";
-        msg += currentDate.getHours()-12+"시";
-    }
-    else {
+        msg += currentDate.getHours() - 12 + "시";
+    } else {
         msg += "오전";
-        msg += currentDate.getHours()+"시";
+        msg += currentDate.getHours() + "시";
     }
-    msg += currentDate.getMinutes()+"분";
-    msg += currentDate.getSeconds()+"초";
+    msg += currentDate.getMinutes() + "분";
+    msg += currentDate.getSeconds() + "초";
     divClock.innerText = msg;
-    if (currentDate.getMinutes()>58) {
-        divClock.style.color="red";
+    if (currentDate.getMinutes() > 58) {
+        divClock.style.color = "red";
     }
     setTimeout(show_clock, 1000);
 }
 
+// 이미지 마우스오버/아웃 시 경로 변경 - 상대경로로 수정
 function over(obj) {
-    obj.src="/java-main/images/LOL_Main2.jpg";
+    obj.src = "./images/LOL_Main2.jpg";
 }
 
 function out(obj) {
-    obj.src="/java-main/images/LOL_Main.jpg";
+    obj.src = "./images/LOL_Main.jpg";
 }
